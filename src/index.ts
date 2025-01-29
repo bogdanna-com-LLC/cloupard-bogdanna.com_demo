c8433225aa39bf5773495e5aa74e409226efab5b
Bogdan Sergeev <bogdansergeevbes@gmail.com>
2025-01-29 15:44:57 +0100

    test 2

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello from cloupard and bogdanna.com! This is a simple HonoJS app powered by Bun.js Test autodeploy')
+  return c.text('Hello from cloupard and bogdanna.com! This is a simple HonoJS app powered by Bun.js Test autodeploy 2')
 })
 
 export default app
